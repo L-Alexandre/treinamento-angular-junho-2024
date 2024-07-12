b3b72baa1f965bb98ed19ce88b3b1a18a5529db3
Luiz Alexandre <121993212+L-Alexandre@users.noreply.github.com>
2024-07-12 03:46:01 -0300

    Removidas abas, mantendo só "Home" e "Profile"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './interfaces/menu-item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'treinamento-angular-junho-2024';
+  items: MenuItem[] = [
+    {
+      'name': 'Home',
+      'url': '/home'
+    },
+    {
+      'name': 'Profiles',
+      'url': '/profiles'
+    },
+  ]
+
+  exibirAlerta(has: string){
+    alert(`O palmeiras ${has} mundial`);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
